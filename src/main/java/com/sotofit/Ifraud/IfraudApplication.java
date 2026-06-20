fef6cd494927ddb7f270fe217377d5b11f55dc25
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2026-06-20 16:41:58 +0100

    application.properties file

--- src/main/java/com/sotofit/Ifraud/IfraudApplication.java
+++ src/main/java/com/sotofit/Ifraud/IfraudApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IfraudApplication {
 
 	public static void main(String[] args) {
+        //Configuration of the dotenv- java
 		SpringApplication.run(IfraudApplication.class, args);
 	}
-
 }
