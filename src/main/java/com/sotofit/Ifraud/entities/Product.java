d5bab4676036ec148ec875ad0e125b929c526564
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2026-06-21 23:06:18 +0100

    chore : Product

--- src/main/java/com/sotofit/Ifraud/entities/Product.java
+++ src/main/java/com/sotofit/Ifraud/entities/Product.java
@@ -1,6 +1,15 @@
 package com.sotofit.Ifraud.entities;
 
+import java.text.DecimalFormat;
+
 public class Product {
-    private String name;
+
+	private String name;
+	private String Description;
+	private DecimalFormat price;
+	private String stock;
+	private String category;
+    private String createdAt;
+    private String UpdatedAt;
 
 }
