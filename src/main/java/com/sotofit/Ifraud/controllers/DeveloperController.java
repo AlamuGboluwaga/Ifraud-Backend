02daf9ecfefd37d514e56e2d5c45e2f369ebc203
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2026-06-24 10:24:29 +0100

    chore: api route

---
+++ src/main/java/com/sotofit/Ifraud/controllers/DeveloperController.java
@@ -0,0 +1,28 @@
+package com.sotofit.Ifraud.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ifraud/")
+public class DeveloperController {
+
+	@Value("${developer.name}")
+	String developerName;
+
+	@Value("${developer.stack}")
+	String developerStack;
+
+	@Value("${age}")
+	int age;
+
+	@Value("${spring.application.name}")
+	String project;
+
+	@GetMapping("/developers")
+	public String developerDetails() {
+		return ("Developer's name  = " + developerName + " and the stack = " + " age = " + age + " project name = "+ project);
+	}
+}
--- src/main/java/com/sotofit/Ifraud/controllers/OrderItemController.java
+++ src/main/java/com/sotofit/Ifraud/controllers/OrderItemController.java
@@ -1,11 +1,13 @@
 package com.sotofit.Ifraud.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 @RequestMapping("/api/order-items")
 public class OrderItemController {
 
