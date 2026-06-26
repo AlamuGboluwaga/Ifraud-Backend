5220a2533092e0d613b1901774c267b86a4b6a64
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2026-06-26 23:46:24 +0100

    Chore: getall users

--- src/main/java/com/sotofit/Ifraud/controllers/TestController.java
+++ src/main/java/com/sotofit/Ifraud/controllers/TestController.java
@@ -27,4 +27,8 @@ public class TestController {
 	public String getUserById() {
 		return coatch.learnBoxing();
 	}
+    @GetMapping
+    public String getUserAll() {
+        return coatch.learnBoxing();
+    }
 }
