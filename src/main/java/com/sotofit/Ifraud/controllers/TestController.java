bbb0254b634493912becfe6eb0641c8fed4779d8
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2026-06-26 23:42:08 +0100

    Chore: @Component

--- src/main/java/com/sotofit/Ifraud/controllers/TestController.java
+++ src/main/java/com/sotofit/Ifraud/controllers/TestController.java
@@ -3,20 +3,28 @@ package com.sotofit.Ifraud.controllers;
 import com.sotofit.Ifraud.test.Coatch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+	// Empty varaible decearation
 	private Coatch coatch;
 
 	@Autowired
 	public TestController(Coatch coatch) {
 		this.coatch = coatch;
 	}
-//This is just for tes
+
+	//This is just for tes
 	@GetMapping("/test")
 	public String getTEst() {
+		return coatch.learnBoxing();
+	}
+
+	@PostMapping
+	public String getUserById() {
 		return coatch.learnBoxing();
 	}
 }
