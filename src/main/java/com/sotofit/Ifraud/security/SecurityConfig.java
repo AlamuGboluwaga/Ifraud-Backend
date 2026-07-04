d03bdca77d6e0dbc40caf20fcf965f11a42127e1
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2026-07-04 21:38:05 +0100

    chore: introduce  lambda method ::

--- src/main/java/com/sotofit/Ifraud/security/SecurityConfig.java
+++ src/main/java/com/sotofit/Ifraud/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.sotofit.Ifraud.security;
 
 import com.sotofit.Ifraud.entities.RegisterUser;
+import org.springframework.boot.micrometer.observation.autoconfigure.ObservationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -24,5 +28,19 @@ public class SecurityConfig {
 		return new InMemoryUserDetailsManager(gboluwaga, adeoye, oluwatosin);
 	}
 
-
+	@Bean
+	public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(configurer ->
+			configurer
+				.requestMatchers(HttpMethod.GET, "/api/**")
+				.hasRole("EMPLOYER")
+				.requestMatchers(HttpMethod.POST, "/api/**")
+				.hasRole("MANAGER")
+				.requestMatchers(HttpMethod.PUT, "/api/**")
+				.hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE, "/api/**")
+				.hasAnyRole("ADMIN")
+		);
+		return null;
+	}
 }
