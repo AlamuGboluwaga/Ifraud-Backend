1420a33741cfac0fb4a1d5caac895a0ea0d56ed6
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2026-07-23 15:24:35 +0100

    chore: Postgres image on Docker

---
+++ BillingServicves/src/main/java/com/example/billingservicves/BillingServicvesApplication.java
@@ -0,0 +1,13 @@
+package com.example.billingservicves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingServicvesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingServicvesApplication.class, args);
+    }
+
+}
